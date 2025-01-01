4284ea7e8902fe8b04ce6541532c0432e45234e6
Aykut Zere <halil.aykut.zere@gmail.com>
2025-01-01 14:47:57 +0300

    upload: global_fn

---
+++ global_fn.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+#|---/ /+------------------+---/ /|#
+#|--/ /-| Global functions |--/ /-|#
+#|-/ /--| Prasanth Rangan  |-/ /--|#
+#|/ /---+------------------+/ /---|#
+
+set -e
+
+scrDir="$(dirname "$(realpath "$0")")"
+cloneDir="$(dirname "${scrDir}")" # fallback, we will use CLONE_DIR now
+cloneDir="${CLONE_DIR:-${cloneDir}}"
+confDir="${XDG_CONFIG_HOME:-$HOME/.config}"
+cacheDir="${XDG_CACHE_HOME:-$HOME/.cache/hyde}"
+aurList=(yay paru)
+shlList=(zsh fish)
+
+export cloneDir
+export confDir
+export cacheDir
+export aurList
+export shlList
+
+pkg_installed() {
+    local PkgIn=$1
+
+    if pacman -Qi "${PkgIn}" &>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+chk_list() {
+    vrType="$1"
+    local inList=("${@:2}")
+    for pkg in "${inList[@]}"; do
+        if pkg_installed "${pkg}"; then
+            printf -v "${vrType}" "%s" "${pkg}"
+            # shellcheck disable=SC2163 # dynamic variable
+            export "${vrType}"
+            return 0
+        fi
+    done
+    return 1
+}
+
+pkg_available() {
+    local PkgIn=$1
+
+    if pacman -Si "${PkgIn}" &>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+aur_available() {
+    local PkgIn=$1
+
+    # shellcheck disable=SC2154
+    if ${aurhlpr} -Si "${PkgIn}" &>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+nvidia_detect() {
+    readarray -t dGPU < <(lspci -k | grep -E "(VGA|3D)" | awk -F ': ' '{print $NF}')
+    if [ "${1}" == "--verbose" ]; then
+        for indx in "${!dGPU[@]}"; do
+            echo -e "\033[0;32m[gpu$indx]\033[0m detected :: ${dGPU[indx]}"
+        done
+        return 0
+    fi
+    if [ "${1}" == "--drivers" ]; then
+        while read -r -d ' ' nvcode; do
+            awk -F '|' -v nvc="${nvcode}" 'substr(nvc,1,length($3)) == $3 {split(FILENAME,driver,"/"); print driver[length(driver)],"\nnvidia-utils"}' "${scrDir}"/nvidia-db/nvidia*dkms
+        done <<<"${dGPU[@]}"
+        return 0
+    fi
+    if grep -iq nvidia <<<"${dGPU[@]}"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+prompt_timer() {
+    set +e
+    unset PROMPT_INPUT
+    local timsec=$1
+    local msg=$2
+    while [[ ${timsec} -ge 0 ]]; do
+        echo -ne "\r :: ${msg} (${timsec}s) : "
+        read -rt 1 -n 1 PROMPT_INPUT && break
+        ((timsec--))
+    done
+    export PROMPT_INPUT
+    echo ""
+    set -e
+}
+
+print_log() {
+    section=${log_section:-hyde}
+    [ -n "${section}" ] && echo -ne "\e[32m[$section] \e[0m"
+    while (("$#")); do
+        # [ "${colored}" == "true" ]
+        case "$1" in
+        -r | +r)
+            echo -ne "\e[31m$2\e[0m"
+            shift 2
+            ;; # Red
+        -g | +g)
+            echo -ne "\e[32m$2\e[0m"
+            shift 2
+            ;; # Green
+        -y | +y)
+            echo -ne "\e[33m$2\e[0m"
+            shift 2
+            ;; # Yellow
+        -b | +b)
+            echo -ne "\e[34m$2\e[0m"
+            shift 2
+            ;; # Blue
+        -m | +m)
+            echo -ne "\e[35m$2\e[0m"
+            shift 2
+            ;; # Magenta
+        -c | +c)
+            echo -ne "\e[36m$2\e[0m"
+            shift 2
+            ;; # Cyan
+        -wt | +w)
+            echo -ne "\e[37m$2\e[0m"
+            shift 2
+            ;; # White
+        -n | +n)
+            echo -ne "\e[96m$2\e[0m"
+            shift 2
+            ;; # Neon
+        -stat)
+            echo -ne "\e[30;46m $2 \e[0m :: "
+            shift 2
+            ;; # status
+        -crit)
+            echo -ne "\e[30;41m $2 \e[0m :: "
+            shift 2
+            ;; # critical
+        -warn)
+            echo -ne "\e[30;43m $2 \e[0m :: "
+            shift 2
+            ;; # warning
+        +)
+            echo -ne "\e[38;5;$2m$3\e[0m"
+            shift 3
+            ;;
+        *)
+            echo -ne "$1"
+            shift
+            ;;
+        esac
+    done
+    echo ""
+}
